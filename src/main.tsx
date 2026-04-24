0b78c5ffcdbfc367d4cf8930f2c398e5c725f163
Kenneth Kwok <kennethkwok@Ashitaka-MacBook-Air.local>
2026-04-24 11:09:20 +0800

    feat: integrate vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App'
 import './index.css'
 import './i18n'
@@ -7,5 +8,6 @@ import './i18n'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
