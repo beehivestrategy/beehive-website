affe17ae7b38713d0e0c1302893d4b56b0bfa727
Kenneth Kwok <kennethkwok@Ashitaka-MacBook-Air.local>
2026-04-24 11:11:21 +0800

    feat: integrate vercel web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import App from './App'
 import './index.css'
 import './i18n'
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
